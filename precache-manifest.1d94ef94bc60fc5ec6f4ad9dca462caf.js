93a4cdc27c7b4a4a6fb72ed683d92fe91cb4d30d
Chris Cuomo <chriscuomo@Chriss-MacBook-Pro.local>
2020-06-25 17:56:53 -0400

    Updates

---
+++ precache-manifest.1d94ef94bc60fc5ec6f4ad9dca462caf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e0ededbbb2828adb7f5141cb681be8",
+    "url": "/monsterTest/index.html"
+  },
+  {
+    "revision": "29963d96cbfc26609259",
+    "url": "/monsterTest/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monsterTest/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "29963d96cbfc26609259",
+    "url": "/monsterTest/static/js/main.360ada18.chunk.js"
+  },
+  {
+    "revision": "8bee5b4c7357bcdaf976",
+    "url": "/monsterTest/static/js/runtime~main.ab7af8a4.js"
+  }
+]);
